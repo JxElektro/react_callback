765d9583f7249d952b893cbb70e1f6d80f19a65c
Jhen Nunez <jxelektro@gmail.com>
2022-08-26 13:11:10 -0400

    26-08

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [coin, setCoin] = useState(0);
+  const [bill, setBill] = useState(0);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> <h1 className='box'> " Todo fue diversion hasta que intente usar callbacks " - Jhen</h1>
+      <div className='box'>
+        <h3>Monedas</h3>
+        <button id='50'  className='coin' onClick={() => setCoin(coin + 50)}>50</button>
+        <button id='100' className='coin' onClick={() => setCoin(coin + 100)}>100</button>
+        <button id='500'  className='coin' onClick={() => setCoin(coin + 500)}>500</button>
+      </div>
+      <div className='box'>
+        <h3>Billetes</h3>
+
+        <button id='1000'  className='bill' onClick={() => setBill(bill + 1000)}>1000</button>
+        <button id='5000'  className='bill' onClick={() => setBill(bill + 5000)}>5000</button>
+        <button id='10000' className='bill' onClick={() => setBill(bill + 10000)}>10000</button>
+      </div>
+      <div className='box'>
+        <h3>Monto Total  :</h3>
+        <h2>{(bill+coin)}</h2>
+      </div>
+      <div className='box'>
+        <h4>Diferencial Monedas:{(coin-bill)} </h4>
+      </div>
+      <div className='box'>
+        <h4>Diferencial Billetes: {(bill-coin)}</h4>
+      </div>
     </div>
   );
 }
---
+++ src/Currency.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import './App.css';
+import App from './App';
+
+
+function buttonClicked() {
+    
+
+
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Currency;
