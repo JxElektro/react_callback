91baa953e5ec5512a9c7a52761af1ad0b53eb5c7
Jhen Nunez <jxelektro@gmail.com>
2022-08-31 00:14:27 -0400

    comentarios

--- src/App.js
+++ src/App.js
@@ -5,29 +5,27 @@ import Box from './box';
 
 function App() {
 
-  const [count, updateCount] = useState(0);
+  const [count, updateCount] = useState(0); // contador padre
 
-  function boxClicked(brother) { //
-    updateCount(count + 1);
-    if (brother === 1) {
+  function boxClicked(gato) { // emulado en box
+    updateCount(count + 1);  // suma contador padre
+    if (gato === 1) {       // suma a alguno de los hijos
       setCount1(count1 + 1);
     } else {
       setCount2(count2 + 1);
     }
-  
-}
+  }
 
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
 
-const [count1, setCount1] = useState(0);
-const [count2, setCount2] = useState(0);
-
-return (
-  <div>
-    <Box id={1} brotherCount={count2} boxPush={boxClicked} />
-    <Box id={2} brotherCount={count1} boxPush={boxClicked} />
-    <p>Contador padre : {count}</p>
-  </div>
-);
+  return (
+    <div>
+      <Box id={1} broCount={count2} Push={boxClicked} /> 
+      <Box id={2} broCount={count1} Push={boxClicked} />
+      <p>Contador padre : {count}</p>
+    </div>
+  );
 }
 
 export default App;
--- src/box.js
+++ src/box.js
@@ -2,22 +2,41 @@ import React, { useState } from 'react';
 
 
 function Box(props) {
-    const [ count , setCount ] = useState(0);
+    const [ count , setCount ] = useState(0);  // contador hijo
 
     function click (){
-      props.boxPush(props.id);
-      setCount (count + 1);
+      props.Push(props.id); // se convierte en boxClicked con el id que corresponda
+      setCount (count + 1); // sin este no se muestra abajo en hijo
     }
 
   return (
     <div onClick={ () => click()}>
-    <div onClick={()=> props.boxPush ()} >
       <p>Caja{props.id} </p>    
       <p>Hijo :{count}</p>   
-      <p>Diferencia con mi hermano : {count - props.brotherCount }</p>
-      </div>
-    </div>
+      <p>Diferencia con mi hermano : {count - props.broCount }</p>
+    </div> 
   );
 }
 
 export default Box;
+
+
+
+/* 
+- al precionar alguna caja se activa la funcion click
+- click determina como push es igual a boxclicked lo emula 
+-se coloca el prop segun el id
+- la funcion le suma
+
+
+
+
+
+
+
+
+
+
+
+
+*/
